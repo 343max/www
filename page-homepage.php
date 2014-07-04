9ec36bd08af6816390e2e50cbfccb9c01e69fb92
Tom Needham <tom@owncloud.com>
2014-07-04 16:20:09 +0100

    Adjustment to new homepage, needs tidy

--- page-homepage.php
+++ page-homepage.php
@@ -2,7 +2,7 @@
 	<div class="row">
 		<div class="col-md-8 col-md-offset-2">
 			<h1 style="font-size: 3em;" >Your data - under your control</h1>
-			<h2 style="font-size: 1.6em; font-weight: 300;">With ownCloud you can sync & share your files, calendar, contacts and more. 
+			<h2 style="margin-top: -10px; margin-bottom: 26px; font-size: 1.6em; font-weight: 300;">With ownCloud you can sync & share your files, calendar, contacts and more. 
 				Access your data from all your devices, on an open platform you can extend and modify.</h2>
 				<a href="/install" class="btn btn-lg btn-default">Download now</a> or 
 				<a href="/providers" class="btn btn-lg btn-default">Find hosting</a>
@@ -15,19 +15,13 @@
 	</div>
 </div>
 <div class="row">
-	<div class="col-md-12" style="text-align: center; padding: 25px 0 35px 0">
+	<div class="col-md-12" style="text-align: center; padding: 25px 0;">
 		<h3><a href="/conf">ownCloud Contributor Conference - August 2014, Berlin</a></h3>
 		<small>The awesome community behind ownCloud will get together at the TU Berlin for 5 days of writing code and sharing beer, knowledge and inspiration.</small>
 	</div>
 </div>
-<div class="row row-blue" style="padding: 40px 0 40px 0">
-	<div class="col-md-2 col-md-offset-2"><a class="btn btn-block btn-lg join-button btn-primary" href="/conf" role="button">Hackathon, Berlin</a></div>
-	<div class="col-md-2"><a class="btn btn-block btn-primary btn-lg join-button" href="/contribute" role="button">Join development</a></div>
-	<div class="col-md-2"><a class="btn btn-block btn-primary btn-lg join-button" href="https://github.com/owncloud/core/issues" role="button">Report an issue</a></div>
-	<div class="col-md-2"><a class="btn btn-block btn-primary btn-lg join-button" href="https://www.transifex.com/projects/p/owncloud/" role="button">Help translating</a></div>
-</div>
 <div class="container" role="document">
-	<div class="row" style="margin-top: 30px;">
+	<div class="row">
 		<main class="main <?php echo roots_main_class(); ?>" role="main">
 
 			<?php if(CONTRIBOOK) { require(dirname(__FILE__).'/../../../contribook/main/contribook/lib_contribook.php'); } ?>
