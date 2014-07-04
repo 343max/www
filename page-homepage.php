01aaa228e70ae154babfea28f4e316c39b65d3b1
Tom Needham <tom@owncloud.com>
2014-07-04 17:41:03 +0100

    More alt homepage updates

--- page-homepage.php
+++ page-homepage.php
@@ -1,16 +1,17 @@
 <div class="home-top-row row-blue">
 	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
+		<div class="col-md-8 col-md-offset-2" style="color: white;">
 			<h1 style="font-size: 3em;" >Your data - under your control</h1>
 			<h2 style="margin-top: -10px; margin-bottom: 26px; font-size: 1.6em; font-weight: 300;">With ownCloud you can sync & share your files, calendar, contacts and more. 
 				Access your data from all your devices, on an open platform you can extend and modify.</h2>
-				<a href="/install" class="btn btn-lg btn-default">Download now</a> or 
+				<a href="/install" class="btn btn-lg btn-primary">Download now</a>&nbsp;&nbsp;or&nbsp;&nbsp;
 				<a href="/providers" class="btn btn-lg btn-default">Find hosting</a>
 		</div>
 	</div>
 	<div class="row" style="padding-bottom: 0; overflow: hidden;">
-		<div class="col-md-8 col-md-offset-2">
-			<img style="width: 90%; margin-bottom: -100px" src="https://cloud.needham.im/public.php?service=files&t=4816bbe6add9f123b8f6f10366006ccf&download"></img>
+		<div class="home-splash col-md-8 col-md-offset-2">
+			<div><img style="width: 90%; margin-bottom: -100px" src="https://cloud.needham.im/public.php?service=files&t=4816bbe6add9f123b8f6f10366006ccf&download"></img></div>
+			<div><a style="opacity: 1 !important; position: absolute; top: 54%; left: 46%;" href="https://demo.owncloud.org" class="btn btn-lg btn-default"><i class="icon-play"></i>&nbsp&nbspDemo</a></div>
 		</div>
 	</div>
 </div>
@@ -20,26 +21,21 @@
 		<small>The awesome community behind ownCloud will get together at the TU Berlin for 5 days of writing code and sharing beer, knowledge and inspiration.</small>
 	</div>
 </div>
-<div class="container" role="document">
-	<div class="row">
-		<main class="main <?php echo roots_main_class(); ?>" role="main">
+<div class="row">
+	<?php if(CONTRIBOOK) { require(dirname(__FILE__).'/../../../contribook/main/contribook/lib_contribook.php'); } ?>
 
-			<?php if(CONTRIBOOK) { require(dirname(__FILE__).'/../../../contribook/main/contribook/lib_contribook.php'); } ?>
-
-			<div class="row 3-column-lg-md-sm">
-				<div class="col-lg-4 col-md-4 col-xs-12 col-sm-4">
-					<h2><i class="icon-twitter"></i> The latest twitter posts</h2>
-					<?php if(CONTRIBOOK) { CONTRIBOOK_MICROBLOG::show(0,3); } ?>
-				</div>
-				<div class="col-lg-4 col-md-4 col-xs-12 col-sm-4">
-					<h2><i class="icon-comment"></i> The latest from the forum</h2>
-					<?php if(CONTRIBOOK) { CONTRIBOOK_FORUM::show(0,7); }?>
-				</div>
-				<div class="col-lg-4 col-md-4 col-xs-12 col-sm-4">
-					<h2><i class="icon-cloud"></i> ownCloud.com news</h2>
-					<?php if(CONTRIBOOK) { CONTRIBOOK_NEWS::show(0,7); } ?>
-				</div>
-			</div>
-		</main><!-- /.main -->
-	</div><!-- /.content -->
-</div><!-- /.wrap -->
+	<div class="col-md-8 col-md-offset-2">
+		<div class="col-md-4">
+			<h2><i class="icon-twitter"></i> The latest twitter posts</h2>
+			<?php if(CONTRIBOOK) { CONTRIBOOK_MICROBLOG::show(0,3); } ?>
+		</div>
+		<div class="col-md-4">
+			<h2><i class="icon-comment"></i> The latest from the forum</h2>
+			<?php if(CONTRIBOOK) { CONTRIBOOK_FORUM::show(0,7); }?>
+		</div>
+		<div class="col-md-4">
+			<h2><i class="icon-cloud"></i> ownCloud.com news</h2>
+			<?php if(CONTRIBOOK) { CONTRIBOOK_NEWS::show(0,7); } ?>
+		</div>
+	</div>
+</div>
